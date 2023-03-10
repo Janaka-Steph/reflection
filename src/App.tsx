02f233f2ea193d814e5c7f633eeccdfc2b5a6364
Janaka-Steph <rochestephane@yahoo.fr>
2023-03-10 15:16:43 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ReflectionServiceClient} from "./api-spec/protobuf/gen/js/reflection/v1/service_pb.client";
+import {createChannel, createClient} from "nice-grpc-web";
 
 function App() {
+  useEffect(() => {
+    const channel = createChannel('http://localhost:9000')
+    const client = createClient(
+      ReflectionServiceClient,
+      channel,
+    );
+    client.getInfo().then((response) => {
+      console.log(response);
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
